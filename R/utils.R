46302b7adf42a9004a81233522b3332daeb61340
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2017-10-15 17:50:07 +0200

    add name support to type checks

--- R/utils.R
+++ R/utils.R
@@ -2,11 +2,22 @@
 is_vec <- function(x,
                    type = c(NA, "int", "num", "lgl", "chr"),
                    n_elem = gte(1L),
+                   names = FALSE,
                    allow_na = FALSE,
                    allow_null = FALSE,
                    extra_test = NULL) {
 
   if (!is.null(extra_test)) stopifnot(rlang::is_quosure(extra_test))
+
+  if (is.logical(names) & length(names) == 1) {
+    check_names <- names
+    if (check_names)
+      names <- names(x)
+    else
+      names <- NULL
+  } else if (is.character(names))
+    check_names <- TRUE
+  else stop("names is expected to either be logical or character")
 
   type <- switch(match.arg(type),
                  int = is.integer,
@@ -14,18 +25,15 @@ is_vec <- function(x,
                  lgl = is.logical,
                  chr = is.character)
 
-  browser()
-
-  rlang::get_env(extra_test)
-
   if (allow_null & is.null(x))
     TRUE
   else if ( !is.vector(x) ||
            (!is.null(type) && !type(x)) ||
+           (check_names &
+             (is.null(names(x)) || !setequal(names(x), names))) ||
            (!allow_na & any(is.na(x))) ||
            (!is.null(n_elem) && !n_elem(length(x))) ||
-           (!rlang::quo_is_null(extra_test) &&
-             rlang::eval_tidy(extra_test, env = child_env())))
+           (!is.null(extra_test) && !rlang::eval_tidy(rlang::UQE(extra_test))))
     FALSE
   else
     TRUE
@@ -34,11 +42,20 @@ is_vec <- function(x,
 is_lgl <- function(...) is_vec(..., type = "lgl")
 is_num <- function(...) is_vec(..., type = "num")
 is_int <- function(..., strict = TRUE)
-  is_vec(..., type = if (strict) "int" else "num")
+  is_vec(..., type = if (strict) "int" else "num",
+         extra_test = if (strict)
+                        NULL
+                      else
+                        rlang::quo(isTRUE(all.equal(x, as.integer(x))))
+         )
 is_chr <- function(..., n_char = gte(1L)) {
   is_vec(..., type = "chr",
-         extra_test = rlang::quo(!is.null(rlang::UQ(n_char)) &&
-                                 !all(rlang::UQ(n_char)(nchar(x[!is.na(x)])))))
+         extra_test = if (is.null(n_char))
+                        NULL
+                      else
+                        rlang::quo(all(rlang::UQ(n_char)(
+                          nchar(x[!is.na(x)]))))
+         )
 }
 
 lt  <- function(x) function(y) y <  x
