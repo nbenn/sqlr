d4ee04514fe04eaef320781ecc52b3dcdcce3ee5
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2017-10-15 16:53:22 +0200

    move n_char check to is_chr only

--- R/utils.R
+++ R/utils.R
@@ -2,9 +2,11 @@
 is_vec <- function(x,
                    type = c(NA, "int", "num", "lgl", "chr"),
                    n_elem = gte(1L),
-                   n_char = NULL,
                    allow_na = FALSE,
-                   allow_null = FALSE) {
+                   allow_null = FALSE,
+                   extra_test = NULL) {
+
+  if (!is.null(extra_test)) stopifnot(rlang::is_quosure(extra_test))
 
   type <- switch(match.arg(type),
                  int = is.integer,
@@ -12,13 +14,18 @@ is_vec <- function(x,
                  lgl = is.logical,
                  chr = is.character)
 
+  browser()
+
+  rlang::get_env(extra_test)
+
   if (allow_null & is.null(x))
     TRUE
   else if ( !is.vector(x) ||
            (!is.null(type) && !type(x)) ||
            (!allow_na & any(is.na(x))) ||
            (!is.null(n_elem) && !n_elem(length(x))) ||
-           (!is.null(n_char) && !all(n_char(nchar(x[!is.na(x)])))))
+           (!rlang::quo_is_null(extra_test) &&
+             rlang::eval_tidy(extra_test, env = child_env())))
     FALSE
   else
     TRUE
@@ -28,8 +35,11 @@ is_lgl <- function(...) is_vec(..., type = "lgl")
 is_num <- function(...) is_vec(..., type = "num")
 is_int <- function(..., strict = TRUE)
   is_vec(..., type = if (strict) "int" else "num")
-is_chr <- function(..., n_char = gte(1L))
-  is_vec(..., type = "chr", n_char = n_char)
+is_chr <- function(..., n_char = gte(1L)) {
+  is_vec(..., type = "chr",
+         extra_test = rlang::quo(!is.null(rlang::UQ(n_char)) &&
+                                 !all(rlang::UQ(n_char)(nchar(x[!is.na(x)])))))
+}
 
 lt  <- function(x) function(y) y <  x
 lte <- function(x) function(y) y <= x
