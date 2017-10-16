67959838b4b78e53d1f5751bd69cc06f20d1fb24
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2017-10-16 22:28:48 +0200

    fix cran checks

--- R/get-spec.R
+++ R/get-spec.R
@@ -46,7 +46,7 @@ get_col_spec.numeric <- function(x, ...) {
 #' @export
 #' 
 get_col_spec.character <- function(x, ...) {
-  lens <- na.omit(nchar(x, "bytes"))
+  lens <- stats::na.omit(nchar(x, "bytes"))
   col_spec(..., type = col_chr, length = max(lens),
            fixed = (length(unique(lens)) == 1L & max(lens) < 256L))
 }
@@ -62,7 +62,7 @@ get_col_spec.list <- function(x, ...) get_col_spec(blob::as.blob(x), ...)
 #' @export
 #' 
 get_col_spec.blob <- function(x, ...) {
-  sizes <- sapply(x, object.size)
+  sizes <- sapply(x, utils::object.size)
   col_spec(..., type = col_raw, length = max(sizes),
            fixed = (length(unique(sizes)) == 1L & max(sizes) < 256L))
 }
