c7f6b4bf86f9a795270893b67abbe56593476e01
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2017-10-16 22:23:25 +0200

    add docs to get_col_spec

--- R/get-spec.R
+++ R/get-spec.R
@@ -1,15 +1,38 @@
+
+#' @title Generate column definition SQL from an R object
+#' 
+#' @description Automatically determine the SQL type and create a column
+#' specification from an R object such as a vector or data.frame/tibble.
+#' 
+#' @name get_col_spec
+#' 
+#' @param x Object for which to generate SQL column specification
+#' @param ... Arguments passed to the S3 methods
+#' 
+#' @return SQL to be used in a CREATE table statement
+#' 
 #' @export
+#' 
 get_col_spec <- function(x, ...) UseMethod("get_col_spec", x)
 
+#' @rdname get_col_spec
+#' 
 #' @export
+#' 
 get_col_spec.integer <- function(x, ...)
   col_spec(..., type = col_int, min = min(x), max = max(x))
 
+#' @rdname get_col_spec
+#' 
 #' @export
+#' 
 get_col_spec.integer64 <- function(x, ...)
   col_spec(..., type = col_int, min = min(x), max = max(x))
 
+#' @rdname get_col_spec
+#' 
 #' @export
+#' 
 get_col_spec.numeric <- function(x, ...) {
   col_spec(..., type = col_dbl,
            prec = if (!is.null(attr(x, "Csingle")) && attr(x, "Csingle"))
@@ -18,44 +41,71 @@ get_col_spec.numeric <- function(x, ...) {
                     "double")
 }
 
+#' @rdname get_col_spec
+#' 
 #' @export
+#' 
 get_col_spec.character <- function(x, ...) {
   lens <- na.omit(nchar(x, "bytes"))
   col_spec(..., type = col_chr, length = max(lens),
            fixed = (length(unique(lens)) == 1L & max(lens) < 256L))
 }
 
+#' @rdname get_col_spec
+#' 
 #' @export
+#' 
 get_col_spec.list <- function(x, ...) get_col_spec(blob::as.blob(x), ...)
 
+#' @rdname get_col_spec
+#' 
 #' @export
+#' 
 get_col_spec.blob <- function(x, ...) {
   sizes <- sapply(x, object.size)
   col_spec(..., type = col_raw, length = max(sizes),
            fixed = (length(unique(sizes)) == 1L & max(sizes) < 256L))
 }
 
+#' @rdname get_col_spec
+#' 
 #' @export
+#' 
 get_col_spec.logical <- function(x, ...)
   col_spec(..., type = col_lgl)
 
+#' @rdname get_col_spec
+#' 
 #' @export
+#' 
 get_col_spec.factor <- function(x, ...)
   col_spec(..., type = col_fct, levels = levels(x))
 
+#' @rdname get_col_spec
+#' 
 #' @export
+#' 
 get_col_spec.POSIXt <- function(x, ...)
   col_spec(..., type = col_dtm, class = "datetime")
 
+#' @rdname get_col_spec
+#' 
 #' @export
+#' 
 get_col_spec.Date <- function(x, ...)
   col_spec(..., type = col_dtm, class = "date")
 
+#' @rdname get_col_spec
+#' 
 #' @export
+#' 
 get_col_spec.difftime <- function(x, ...)
   col_spec(..., type = col_dtm, class = "time")
 
+#' @rdname get_col_spec
+#' 
 #' @export
+#' 
 get_col_spec.data.frame <- function(x, ...) {
 
   dots <- list(...)
