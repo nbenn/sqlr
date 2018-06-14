f8098ebbd0cdb698541a7493990db70ec83dc2f4
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-06-14 16:38:57 +0200

    add dialect specific type

--- R/mysql.R
+++ R/mysql.R
@@ -8,12 +8,12 @@
 
 #' @export
 enable_mysql <- function() {
-  numeric$set("private", "length", NA)
+  numeric$set("private", "display_width", NA)
   numeric$set(
     "public",
     "initialize",
-    function(length = NA) {
-      private$length <- length
+    function(display_width = NA) {
+      private$display_width <- display_width
     }
   )
 }
@@ -25,11 +25,20 @@ mysql_type_composer <- R6Class(
   public = list(
     generate_sql = function(x) {
       res <- super$generate_sql(x)
-      if (!is.na(x$length)) {
-        assert_that(is.count(x$length))
-        paste0(res, "(", x$length, ")")
+      if (!is.na(x$display_width)) {
+        assert_that(is.count(x$display_width))
+        paste0(res, "(", x$display_width, ")")
       } else
         res
     }
+  )
+)
+
+#' @export
+mysql_tiny_int <- R6Class(
+  "tiny_int",
+  inherit = integer,
+  private = list(
+    sql_type = "TINYINT"
   )
 )
