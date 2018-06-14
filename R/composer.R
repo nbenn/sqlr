e60946e98c8a3bb5209f76295e0a29bf50d75fd2
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-06-14 14:10:51 +0200

    experiment with R6 class hierarchies

---
+++ R/composer.R
@@ -0,0 +1,19 @@
+
+composer <- R6Class(
+  "composer",
+  public = list(
+    generate_sql = function(...) {
+      stop("implement in child classes")
+    }
+  )
+)
+
+#' @export
+type_composer <- R6Class(
+  "type_composer",
+  public = list(
+    generate_sql = function(x) {
+      paste(x$sql_type)
+    }
+  )
+)
---
+++ R/data-types.R
@@ -0,0 +1,55 @@
+#' @examples
+#' int <- integer$new()
+#' int$as_sql()
+#' 
+#' @importFrom R6 R6Class
+
+data_type <- R6Class(
+  "data_type",
+  public = list(
+    as_r = function(...) {
+      stop("implement in child classes")
+    },
+    as_sql = function(composer = type_composer$new()) {
+      composer$generate_sql(private)
+    }
+  ),
+  private = list(
+    sql_type = NULL
+  )
+)
+
+numeric <- R6Class(
+  "numeric",
+  inherit = data_type
+)
+
+#' @export
+integer <- R6Class(
+  "integer",
+  inherit = numeric,
+  public = list(
+    as_r = as.integer
+  ),
+  private = list(
+    sql_type = "INTEGER"
+  )
+)
+
+#' @export
+small_int <- R6Class(
+  "small_int",
+  inherit = integer,
+  private = list(
+    sql_type = "SMALLINT"
+  )
+)
+
+#' @export
+big_int <- R6Class(
+  "big_int",
+  inherit = integer,
+  private = list(
+    sql_type = "BIGINT"
+  )
+)
