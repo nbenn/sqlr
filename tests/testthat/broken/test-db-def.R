3322c7649c02136c873ef74690b6d87f9f4bb881
Kirill Müller <krlmlr+r@mailbox.org>
2018-09-21 15:55:13 +0200

    test-db-def

--- tests/testthat/broken/test-db-def.R
+++ tests/testthat/broken/test-db-def.R
@@ -1,12 +1,16 @@
 context("data definition functions")
 
-set_con(section = "mysql_unittest")
+setup(
+  set_con(section = "mysql_unittest")
+)
 
 test_that("mysql tables can be created", {
-  expect_true(create_db_tbl("iris", get_col_spec(iris)))
-  expect_true("iris" %in% DBI::dbListTables(sqlr:::get_con()))
+  expect_true(create_db_tbl("iris", get_col_spec(iris), con = get_con()))
+  expect_true("iris" %in% DBI::dbListTables(get_con()))
   expect_true(all(colnames(iris) == show_db_cols("iris")$Field))
   expect_true(drop_db_tbl("iris"))
 })
 
-rm_con()
+teardown(
+  rm_con()
+)
